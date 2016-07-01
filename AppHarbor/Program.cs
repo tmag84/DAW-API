33435f22617653bc6cc0f273844ea3c4ded5b5ca
tmag84 <28593@alunos.isel.ipl.pt>
2016-07-02 00:17:27 +0100

    deploy

---
+++ AppHarbor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Hosting;
+using DAWApi;
+using System;
+
+
+namespace AppHarbor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("localhost:8080"))
+            {
+                Console.WriteLine("Server is listening...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
