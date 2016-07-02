146d5410415a2ac8b9b206ac0a0cded15088fb59
tmag84 <28593@alunos.isel.ipl.pt>
2016-07-02 15:33:13 +0100

    redeploy

---
+++ AppHarbor/AppHarbor/Controllers/CommentsController.cs
@@ -0,0 +1,62 @@
+﻿using AppHarbor.Models;
+using AppHarbor.Utils;
+using AppHarbor.Utils.DB;
+using AppHarbor.Utils.Exceptions;
+using Drum;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace AppHarbor.Controllers
+{
+    [RoutePrefix(Const_Strings.COMMENT_ROUTE_PREFIX)]
+    public class CommentsController : ApiController
+    {
+        [HttpPut, Route("edit-comment/{cid}")]
+        public HttpResponseMessage EditComment(string name, int id, int cid, CommentModel model)
+        {
+            HttpResponseMessage resp;
+            model.Id = cid;
+            var uriMaker = Request.TryGetUriMakerFor<CommentsController>();
+            try
+            {
+                DB_Puts.PutComment(name, id, model);
+                resp = new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (MyException e)
+            {
+                ErrorModel error = e.GetError();
+                error.instance = uriMaker.UriFor(c => c.EditComment(name, id, cid, model)).AbsoluteUri;
+                resp = Request.CreateResponse<ErrorModel>(
+                                    error.status, error,
+                                    new JsonMediaTypeFormatter(),
+                                    new MediaTypeHeaderValue("application/problem+json"));
+            }
+            return resp;
+        }
+
+        [HttpPost, Route("create-comment")]
+        public HttpResponseMessage PostComment(string name, int id, CommentModel model)
+        {
+            HttpResponseMessage resp;
+            var uriMaker = Request.TryGetUriMakerFor<CommentsController>();
+            try
+            {
+                DB_Posts.PostComment(name, id, model);
+                resp = new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch (MyException e)
+            {
+                ErrorModel error = e.GetError();
+                error.instance = uriMaker.UriFor(c => c.PostComment(name, id, model)).AbsoluteUri;
+                resp = Request.CreateResponse<ErrorModel>(
+                                    error.status, error,
+                                    new JsonMediaTypeFormatter(),
+                                    new MediaTypeHeaderValue("application/problem+json"));
+            }
+            return resp;
+        }
+    }
+}
---
+++ AppHarbor/AppHarbor/Models/ProjectsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebApi.Hal;
+
+namespace AppHarbor.Models
+{    
+    public class ProjectsModel : SimpleListRepresentation<ProjectModel>
+    {
+        public ProjectsModel(IList<ProjectModel> projects): base(projects){
+        }
+    }
+}
