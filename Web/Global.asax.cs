6ac66041188058d0017dd548568718ddf2416b5f
tmag84 <28593@alunos.isel.ipl.pt>
2016-07-02 18:47:30 +0100

    fixes

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using WebApi;
 
 namespace AppHarbor.Web
 {
@@ -12,6 +14,7 @@ namespace AppHarbor.Web
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
