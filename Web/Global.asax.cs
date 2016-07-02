60f7ec0f5a5a91f9b281dfaf3a05b9e5b6b5b3f1
tmag84 <28593@alunos.isel.ipl.pt>
2016-07-02 19:04:37 +0100

    fixes

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -14,7 +14,6 @@ namespace AppHarbor.Web
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
